038bfc24bfc3b90cfa8580e0483ab6110eb21898
karlarevelo <karla.revelo01@epn.edu.ec>
2020-08-21 19:52:46 -0500

    Igualación clases

--- 03 - backend/mascotas/api/models/Usuario.js
+++ 03 - backend/mascotas/api/models/Usuario.js
@@ -6,24 +6,39 @@
  */
 
 module.exports = {
-
+  tableName:'epn_usuario',
   attributes: {
+    cedula:{ //nombre del atributo
+      type:'String',
+      required:true,
+      columnName: 'epn_cedula',
+      unique:true,
+      minLength:10,
+      maxLength:25,
+    },
+    nombre:{
+      type:'String',
+      required:true,            
+      minLength:3
+    },
+    correo:{
+      type:'String',
+      isEmail:true
+    },
+    estadoCivil:{
+      type:'String',
+      isIn:['Soltero','Casado','Divorciado','Viudo','Union Libre'],
+      defaultsTo:'Soltero'
+    },
+    password:{
+      type:'String',
+      regex:/^[a-zA-Z]\w{3,14}$/
+    },
 
-    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
-    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
-    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
-
-
-    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
-    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
-    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
-
-
-    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
-    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
-    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    Pokemons:{//one to many (plural)
+      collection: "pokemon", //referenci al modelo
+      via:"usuario" //Nombre fk en pokemon
+    }
 
   },
-
 };
-
